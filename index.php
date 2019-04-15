397dd6e442c800a712135c0e2e83c8b9e21662a4
Daniil Bondar <24-test@bk.ru>
2019-04-15 22:08:14 +0400

    Beta 2

--- index.php
+++ index.php
@@ -16,7 +16,14 @@ require_once 'includes/header.inc.php';
 <h3>Добро пожаловать!<h3>
 </div><br>
 <div class="alert alert-warning" role="alert">
-<h4>Информация по счетчикам - на вкладке "Счетчики"</h4>
+<h5>Информация по счетчикам - на вкладке "Счетчики"</h5>
+<h5>Заявки на установку / обслуживание - на вкладке "Заявки"</h5>
+<h5>Баланс - в меню "Аккаунт"</h5>
+</div><br>
+<div class="alert alert-success" role="alert">
+<h5>Сделано</h5>
+<h3>Payalnik Team</h3>
+<h5><a href="http://payalnik-team.ru/">payalnik-team.ru</a></h5>
 </div><br>
 </center>
 </body>
