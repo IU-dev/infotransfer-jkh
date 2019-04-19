075b058e18db6f7917f0a33225be9f32d38a0c89
Daniil Bondar <24-test@bk.ru>
2019-04-20 00:02:17 +0400

    R1

--- z_counter.php
+++ z_counter.php
@@ -105,9 +105,7 @@ $error = '<div class="alert alert-success" role="alert">Запрос выпол�
             '</thead>';
 	$i = 1;
 	foreach($counters as $counter){
-		echo 'iteration';
 		if($counter['state'] == "0"){
-			echo 'okay';
 			echo '<tr>';
 			echo '<td>'.$i.'</td>';
 			echo '<td>'.$counter['id'].'</td>';
