2db44d1c2a7e05190d9bc8c7b0a632c71f39362a
Daniil Bondar <24-test@bk.ru>
2019-06-21 23:57:14 +0300

    R1.1 (release commit 2)

--- logout.php
+++ logout.php
@@ -2,7 +2,6 @@
 //logout.php
 require_once 'includes/global.inc.php';
 $page = "index.php";
-require_once 'includes/header.inc.php';
 $userTools = new UserTools();
 $userTools->logout();
 header("Location: index.php");
